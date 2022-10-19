e9441c3f30d37f665a16bed70dc369b2dc0dcf4b
Varun Raikar <73928389+Ueuu@users.noreply.github.com>
2022-10-19 17:18:48 +0530

    initial

---
+++ WebAPI/Deploy/API/API/Data/CardsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class CardsDbContext : DbContext
+    {
+        public CardsDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        //Dbset
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
